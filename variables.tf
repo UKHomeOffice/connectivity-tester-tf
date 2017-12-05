6ff50eb6fb6aba022ed5635cd7a547339e6e8899
Chris Nesbitt-Smith <chris@cns.me.uk>
2017-12-05 18:28:08 +0000

    remove vpc sg param

--- variables.tf
+++ variables.tf
@@ -14,11 +14,6 @@ variable associate_public_ip_address {
   default = false
 }
 
-variable vpc_security_group_ids {
-  type    = "list"
-  default = []
-}
-
 variable security_groups {
   type    = "list"
   default = []
